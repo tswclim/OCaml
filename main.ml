11b9a7cbb7369b2054bdc6426ae46e3833155a3d
LiM <tswclim@163.com>
2024-10-04 08:03:20 +0800

    ssh

--- main.ml
+++ main.ml
@@ -9,3 +9,7 @@ print_string "hotfix";
 print_string "hotfix";
 print_string "hotfix";
 
+print_string "master------------";
+print_string "master------------";
+print_string "master------------";
+print_string "ssh";
